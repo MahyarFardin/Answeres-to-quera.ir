ed770e151dc8205a776aee2851dca3e57f834939
Mahyar Fardinfar <95848199+MahyarFardin@users.noreply.github.com>
2022-01-27 10:18:39 +0330

    Create Main.java

---
+++ quera answeres/مثلث خیام پاسکال/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class java {
+    public static int [][] Khayam(int [][] triangle,int row,int column){
+        triangle[row][column]=triangle[row-1][column-1] + triangle[row-1][column];
+        column++;
+        if (triangle[row][column] ==1) {
+            row++;
+            column=1;
+            if (row==triangle.length) {
+                return triangle;
+            }
+        }
+
+        return triangle=Khayam(triangle,row,column);
+    }
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int number=scan.nextInt();
+
+        int [][] triangle=new int[number][number];
+
+        for (int i = 0; i < number; i++) {
+            triangle[i][i]=1;
+            triangle[i][0]=1;
+        }
+
+        triangle=Khayam(triangle,2,1);
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print(triangle[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
