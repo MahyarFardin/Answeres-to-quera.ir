55096e416f1a1e97a169b77668749824b7022626
Mahyar Fardinfar <95848199+MahyarFardin@users.noreply.github.com>
2022-01-27 10:38:13 +0330

    Update Main.java

--- quera answeres/یافتن عدد اول/Main.java
+++ quera answeres/یافتن عدد اول/Main.java
@@ -9,7 +9,7 @@ public class java {
             return false;
         }
         else {
-            for (int i = 3; i < Math.sqrt(n); i += 2) {
+            for (int i = 3; i <=Math.sqrt(n); i += 2) {
                 if (n%i == 0) {
                     return false;
                 }
