4117f7de42c289925553e549e538fae6a4c08801
Mahyar Fardinfar <95848199+MahyarFardin@users.noreply.github.com>
2022-01-31 11:08:52 +0330

    Create Main.java

---
+++ quera answeres/خوش آمد/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class java {
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        System.out.println("Hello CodeCup "+n+"!");
+    }
+}
