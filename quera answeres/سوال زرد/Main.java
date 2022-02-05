c07ab32f7b7c851c6a5bdd7a9ca00a4937453d4d
Mahyar Fardinfar <95848199+MahyarFardin@users.noreply.github.com>
2022-02-05 15:24:09 +0330

    Create Main.java

---
+++ quera answeres/سوال زرد/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class java {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        System.out.print("W");
+        for (int i = 0; i < n; i++) {
+            System.out.print("o");
+        }
+        System.out.println("w!");
+    }
+}
