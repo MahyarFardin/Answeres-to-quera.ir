85d3367391c6d1dfa2c130ee6ea50f3bcb9e6c3c
Mahyar Fardinfar <95848199+MahyarFardin@users.noreply.github.com>
2022-02-07 21:33:47 +0330

    Create Main.java

---
+++ quera answeres/یک سوال ساده/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class java {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int a= scan.nextInt();
+        for (int i = 0; i < a; i++) {
+            System.out.println("man khoshghlab hastam");
+        }
+    }
+}
