3dbbc3803fde772bab9c05f7cc2897e773f908b6
Mahyar Fardinfar <95848199+MahyarFardin@users.noreply.github.com>
2022-02-09 19:39:36 +0330

    Create Main.java

---
+++ quera answeres/شارژ موبایل/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class java {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+        int x= scan.nextInt();
+        System.out.println((int)((1+x)*(x/2.0)));
+
+    }
+}
