1a6b12a5505a16be81caf74846972fbcf421dc5e
mahyarfardin <mahyarfardinfar@gmail.com>
2022-03-04 17:34:20 +0330

    done the circle

--- university assignments/java/src/Point.java
+++
@@ -1,9 +0,0 @@
-public class Point {
-    private double m_x;
-    private double m_y;
-
-    Point(double x , double y){
-        m_x=x;
-        m_y=y;
-    }
-}
--- university assignments/java/src/Size.java
+++
@@ -1,9 +0,0 @@
-public class Size {
-    private double m_width;
-    private double m_height;
-
-    Size(double width, double height){
-        m_width=width;
-        m_height=height;
-    }
-}
