91b1b57b62a2b4e6105f2aca3db9fdffe8e31e82
mahyarfardin <mahyarfardinfar@gmail.com>
2022-03-04 17:59:34 +0330

    will be added

--- university assignments/Second semester/2D-World/src/Size.java
+++ university assignments/Second semester/2D-World/src/Size.java
@@ -6,4 +6,22 @@ public class Size {
         m_width=width;
         m_height=height;
     }
+
+    public void setM_height(double m_height) {
+        this.m_height = m_height;
+    }
+    public void setM_width(double m_width) {
+        this.m_width = m_width;
+    }
+    public double getM_height() {
+        return m_height;
+    }
+    public double getM_width() {
+        return m_width;
+    }
+
+    public void Show(){
+        System.out.println("Height is: "+m_height);
+        System.out.println("Width is: "+m_width);
+    }
 }
